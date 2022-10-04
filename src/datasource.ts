2e27bd76f1902bf6fb29384e50f65f2591e562b8
Scott McKittrick <samckittrick@users.noreply.github.com>
2022-10-04 05:15:38 +0000

    Added support for time ranges Added field types

--- src/datasource.ts
+++ src/datasource.ts
@@ -27,21 +27,30 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   }
 
   // TODO: Add default query
+  // query doesn't need to be async anymore because we are using Observables
   query(options: DataQueryRequest<MyQuery>): Observable<DataQueryResponse> {
-    console.log("In Query");
     const observableResponses: Array<Observable<DataQueryResponse>> = options.targets.map((query) => {
-      const apiPath = `/api/v3/datasets/${query.database_code}/${query.dataset_code}/data.json`
+
+      // Build query parameters and path
+      const { range } = options;
+      const from = range!.from.format("YYYY-MM-DD");
+      const to = range!.to.format("YYYY-MM-DD");
+      const apiPath = `/api/v3/datasets/${query.database_code}/${query.dataset_code}/data.json?start_date=${from}&end_date=${to}`;
+      console.log(apiPath);
+      // Create a new observable to return
       const queryObservable = new Observable<DataQueryResponse>((subscriber) => {
-        console.log(`In observable for refid ${query.refId}`)
+
+        // The doRequest function returns an observable. We subscribe to it, format the data
+        // and emit our own next with the formatted data. 
         let response: Observable<FetchResponse> =  this.doRequest(apiPath);
-        console.log(response)
-        console.log(`${query.refId}: Got response`)
         let respSubscriber = response.subscribe({
           next(r) { 
-            console.log(`${query.refId}: In next of respSubscriber`);
             console.log(r);
 
-            if(r.status !== 200) {throw new Error(`Unexpected HTTP Response from API: ${r.status} - ${r.statusText}`);}
+            if(r.status !== 200) {
+              subscriber.error(`Unexpected HTTP Response from API: ${r.status} - ${r.statusText}`);
+              return;
+            }
 
             // Start Parsing the Response
             let df = new MutableDataFrame({
@@ -51,23 +60,33 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
 
             let dataset_data: QuandlDataset = r.data.dataset_data as QuandlDataset;
             for(const f of dataset_data.column_names) {
-              df.addField({ name: f, type: FieldType.string})
+              
+              // The time series data set always has a date and then number fields. 
+              // With tables we'll probably have to infer data types or just use xml because the xml format shows types. . 
+              if(f === "Date") {
+                df.addField({name: f, type: FieldType.time})
+              } else {
+                df.addField({ name: f, type: FieldType.number})
+              }
             }
 
             for(const r of dataset_data.data) {
               df.appendRow(r);
             }
-
+            
+            // Alert the subscriber that we have new formatted data. 
+            // Not sure why I have to put it in an object with the array, but it seems to work. 
             subscriber.next( {data: [df] } ); 
             
           },
-          error(err) { console.log(err) },
+          error(err) { 
+            console.log(err);
+            subscriber.error(`API returned error: ${err.status} - ${err.statusText}`);
+          },
           complete() { 
-            console.log("Complete")
+            // Once we are done reading the response, we can call it complete here too. 
             subscriber.complete(); 
-            console.log(`${query.refId}: Unsubscribing`)
             respSubscriber.unsubscribe()
-            console.log(`${query.refId}: Unsubscribed`)
           }
         });
 
@@ -76,7 +95,7 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
       return queryObservable;
     });
 
-    // ToDo Check what this function does.
+    // The query function only returns one observable. we use merge to combine them all?
     return merge(...observableResponses);
     
 
